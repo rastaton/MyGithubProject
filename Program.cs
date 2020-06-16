863c0a0b63282ff6c189eb2ba9082679fd6e5249
rastaton <66689912+rastaton@users.noreply.github.com>
2020-06-16 14:24:19 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,107 +1,47 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 
-public class Example
+
+
+namespace Lab3_5rstaton
+
 {
-    public static void Main()
+
+    class Program
+
     {
-        string pattern = @"[d-e]";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
 
-        foreach (Match m in Regex.Matches(input, pattern, options))
+        static void Main(string[] args)
+
         {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+
+            string blackListWord = "increase";
+
+            bool isSpam = false;
+
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+
+            {
+
+                isSpam = true;
+
+                Console.WriteLine("The message is spam");
+
+            }
+
+            else
+
+            {
+
+                isSpam = false;
+
+                Console.WriteLine("The message is not spam.");
+
+            }
+
         }
+
     }
-}
 
-using System;
-using System.Text.RegularExpressions;
-
-public class Example
-{
-    public static void Main()
-    {
-        string pattern = @"[d-e][*d]";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
-        {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-        }
-    }
-}
-
-using System;
-using System.Text.RegularExpressions;
-
-public class Example
-{
-    public static void Main()
-    {
-        string pattern = @"[a][a-z][a]";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
-        {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-        }
-    }
-}
-
-using System;
-using System.Text.RegularExpressions;
-
-public class Example
-{
-    public static void Main()
-    {
-        string pattern = @"[ ][p][a-z]*";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
-        {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-        }
-    }
-}
-
-using System;
-using System.Text.RegularExpressions;
-
-public class Example
-{
-    public static void Main()
-    {
-        string pattern = @"[a-z]*[e][ ]";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
-        {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-        }
-    }
-}
-
-using System;
-using System.Text.RegularExpressions;
-
-public class Example
-{
-    public static void Main()
-    {
-        string pattern = @"[A-Z]";
-        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…. ";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
-        {
-            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
-        }
-    }
 }
