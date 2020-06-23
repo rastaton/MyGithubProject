f656f6e35a01aba2566da8dea921c1a270ad85d8
Richard Staton <rncstaton@hotmail.com>
2020-06-23 12:47:41 -0500

    Lab2_5

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number1; 
+
+            int number2; 
+
+
+
+            Console.WriteLine("Enter a value for the first number:");
+
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for the second number:");
+
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int result = number1 + number2;
+
+            Console.WriteLine("The answer is:");
+
+            Console.WriteLine(result);
+
+
+
+            result = number1 - number2;
+
+            Console.WriteLine("The answer is:");
+
+            Console.WriteLine(result);
+
+
+
+
+
+            result = number1 * number2;
+
+            Console.WriteLine("The answer is:");
+
+            Console.WriteLine(result);
+
+
+
+            double results2 = (double)number1 / (double)number2;   
+
+            double results3 = number1 % number2;  
+
+            Console.WriteLine("The answer is:");
+
+            Console.WriteLine(results2);  
+
+            Console.WriteLine(results3);
+        }
+    }
+}
